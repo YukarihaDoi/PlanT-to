b86735f5cc8e18e270212776c58bd77a9e82b6ae
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-08-05 07:54:57 +0000

    [Add]カラム,ActiveStorage

--- db/migrate/20220805062946_create_post_images.rb
+++ db/migrate/20220805062946_create_post_images.rb
@@ -1,7 +1,8 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-
+      t.string :title, null: false
+      t.text :body, null: false
       t.timestamps
     end
   end
