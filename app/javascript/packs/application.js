aea410204ea94dab18b7935b383f6f83bd416f45
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-08-05 01:59:10 +0000

    [Add]BootStrap,FontAwsome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,15 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// BootStrap導入12-15 FontAwesome16
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
+
+// 追加したcssファイルをimport で追加する
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
