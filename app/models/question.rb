e91df2b92ffcc626be6e6af9c98abf1f67af0ea8
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-08-12 03:09:36 +0000

    [Add]アソシエーション

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,17 @@
 class Question < ApplicationRecord
+  # アソシエーション
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  # 画像
   has_one_attached :question_image
+
+  # 画像確認/サイズ
+  def get_question_image
+    unless question_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      question_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    question_image.variant(resize_to_limit: [100, 100]).processed
+  end
+
 end
