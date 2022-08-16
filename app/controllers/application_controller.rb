abf12f06d73ecbdfd2c311bca47338fc5506e4bb
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-08-16 23:40:41 +0000

    [Add]管理者ヘッター

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,12 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   # サインイン後の遷移先
   def after_sign_in_path_for(resource)
-    post_images_path
+    if user_signed_in?
+      post_images_path
+    else
+      users_path
+    end
   end
-
   protected
 
   def configure_permitted_parameters
