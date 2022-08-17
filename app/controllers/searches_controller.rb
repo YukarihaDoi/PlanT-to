baaad8544f2b42fd8424b8bc99136dfeef637427
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-08-17 03:38:01 +0000

    [Add]ユーザー検索

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,5 +2,7 @@ class SearchesController < ApplicationController
   def search
     @post_images = PostImage.where("title LIKE?", "%#{params[:word]}%")
     @post_images_all = PostImage.all
+    @users = User.where("name LIKE?", "%#{params[:word]}%")
+    @usars_all = User.all
   end
 end
