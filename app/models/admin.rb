5b8427f2a3deefd102bc1ef0b64f35bd9a4363a4
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-08-30 23:21:08 +0000

    [Add]画像取り消し

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,16 +3,16 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  ##画像
-  has_one_attached :admin_profile_image
+  # ##画像
+  # has_one_attached :admin_profile_image
 
-  # 画像確認/サイズ
-  def admin_profile_image(width, height)
-    unless admin_profile_image.attached?
-      # ここの画像直す
-      file_path = Rails.root.join('app/assets/images/no-image_user.jpg')
-      admin_profile_image.attach(io: File.open(file_path), filename: 'no-image_user.jpg', content_type: 'image/jpeg')
-    end
-    news_image.variant(resize_to_limit: [width, height]).processed
-  end
+  # # 画像確認/サイズ
+  # def admin_profile_image(width, height)
+  #   unless admin_profile_image.attached?
+  #     # ここの画像直す
+  #     file_path = Rails.root.join('app/assets/images/no-image_user.jpg')
+  #     admin_profile_image.attach(io: File.open(file_path), filename: 'no-image_user.jpg', content_type: 'image/jpeg')
+  #   end
+  #   admin_profile_image.variant(resize_to_limit: [width, height]).processed
+  # end
 end
