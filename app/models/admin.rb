86880ce45684d610ffdb8b6906d9165988eecc22
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-08-30 14:52:56 +0000

    [Fix]モデル記述

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -8,10 +8,10 @@ class Admin < ApplicationRecord
 
   # 画像確認/サイズ
   def admin_profile_image(width, height)
-    unless news_image.attached?
+    unless admin_profile_image.attached?
       # ここの画像直す
       file_path = Rails.root.join('app/assets/images/no-image_user.jpg')
-      news_image.attach(io: File.open(file_path), filename: 'no-image_user.jpg', content_type: 'image/jpeg')
+      admin_profile_image.attach(io: File.open(file_path), filename: 'no-image_user.jpg', content_type: 'image/jpeg')
     end
     news_image.variant(resize_to_limit: [width, height]).processed
   end
